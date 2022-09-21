429968dd5e2be4bfd15df6f16e8188134f808d32
Wladzi0 <jiayug4sv@gmail.com>
2022-09-21 16:07:36 +0200

    connect with free API and get data

---
+++ AutoService/ApiLibrary/ApiProcessor.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace ApiLibrary
+{
+    public class ApiProcessor
+    {
+        public static void LoadBasicCarContent()
+        {
+            string makeUrl = "https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvaluesextended/5UXWX7C5*BA?format=json&modelyear=2011";
+
+            HttpWebRequest request = WebRequest.CreateHttp(makeUrl);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            string responseToString;
+            using (StreamReader streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                responseToString = streamReader.ReadToEnd();
+                
+            }
+             //JObject obj = JObject.Parse(responseToString);
+
+             //var make = (string)obj.SelectToken("Results[0].Model");
+          MakeModel make = JsonConvert.DeserializeObject<MakeModel>(responseToString);
+
+        }
+      
+    }
+}
---
+++ AutoService/ApiLibrary/VehicleData.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoService.ApiLibrary
+{
+    public class VehicleData
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int ModelYear { get; set; }
+    }
+}
